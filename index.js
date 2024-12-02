8867fc38c4a44b421bedc720e82e902cf9494ca3
MateoMendez07 <henry.mendez549@ist17dejulio.edu.ec>
2024-12-02 09:21:44 -0500

    hola mundo servidor

---
+++ index.js
@@ -0,0 +1,18 @@
+//protocolo de intercambio
+var http = require('http');
+//crear servidor
+var server = http.createServer();
+//servicio web
+function mensaje(req,resp){
+    //estado satisfactorio 200, mensaje tipo texto plano configurado por json 
+    resp.writeHead(200,{'contentType':'text/plain'});
+    //escribo el mensaje
+    resp.write('HOLA MUNDO');
+    //finalizar la comunicacion entre cliente y servidor
+    resp.end();
+}
+
+server.on('request',mensaje);
+server.listen(3000,function(){
+    console.log("La aplicación esta corriendo mediante el puerto 3000");
+});
